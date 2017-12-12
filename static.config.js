93d8ac7929922ca034b5721fc7c3b634fec6abe8
yuyaohshimo <g2110008@gmail.com>
2017-12-13 01:18:58 +0900

    Custom postcss

--- static.config.js
+++ static.config.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
+import autoprefixer from 'autoprefixer'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import postcssFlexbugsFixes from 'postcss-flexbugs-fixes'
+
 export default {
   getSiteProps: () => ({
     title: 'React Static',
@@ -34,5 +38,104 @@ export default {
         component: 'src/containers/404',
       },
     ]
+  },
+  webpack: (config, { stage, defaultLoaders }) => {
+    let cssLoader = {}
+
+    if (stage === 'dev') {
+      cssLoader = {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              sourceMap: true,
+              ident: 'postcss',
+              plugins: () => [
+                postcssFlexbugsFixes,
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+        ],
+      }
+    } else {
+      cssLoader = {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: {
+            loader: 'style-loader',
+            options: {
+              sourceMap: false,
+              hmr: false,
+            },
+          },
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                minimize: true,
+                sourceMap: false,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                // Necessary for external CSS imports to work
+                // https://github.com/facebookincubator/create-react-app/issues/2677
+                sourceMap: true,
+                ident: 'postcss',
+                plugins: () => [
+                  postcssFlexbugsFixes,
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9', // React doesn't support IE8 anyway
+                    ],
+                    flexbox: 'no-2009',
+                  }),
+                ],
+              },
+            },
+          ],
+        }),
+      }
+    }
+
+    config.module.rules = [
+      {
+        oneOf: [
+          {
+            test: /\.json$/,
+            use: [{ loader: 'json-loader' }],
+          },
+          defaultLoaders.jsLoader,
+          cssLoader,
+          defaultLoaders.fileLoader,
+        ],
+      },
+    ]
+    return config
   },
 }
